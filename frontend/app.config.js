3052cb9aba82099331cf56a05ae1da18ba631b2c
rafael-andrade-qa <142061101+rafael-iftm@users.noreply.github.com>
2025-05-27 20:17:38 -0300

    Update app.config.js

--- frontend/app.config.js
+++ frontend/app.config.js
@@ -2,43 +2,46 @@ import 'dotenv/config';
 
 export default {
   expo: {
-    name: 'avalia-kids',
+    name: 'Avalia Kids',
     slug: 'avalia-kids',
     version: '1.0.0',
     orientation: 'portrait',
     icon: 'https://firebasestorage.googleapis.com/v0/b/avaliakids.firebasestorage.app/o/logo.png?alt=media',
     scheme: 'myapp',
-    userInterfaceStyle: 'automatic',
+    userInterfaceStyle: 'light',
     newArchEnabled: true,
+
+    splash: {
+      image: 'https://firebasestorage.googleapis.com/v0/b/avaliakids.firebasestorage.app/o/logo.png?alt=media',
+      resizeMode: 'contain',
+      backgroundColor: '#ffffff',
+    },
+
     ios: {
       supportsTablet: true,
     },
+
     android: {
       adaptiveIcon: {
         foregroundImage: 'https://firebasestorage.googleapis.com/v0/b/avaliakids.firebasestorage.app/o/logo.png?alt=media',
         backgroundColor: '#ffffff',
       },
     },
+
     web: {
       bundler: 'metro',
       output: 'static',
       favicon: 'https://firebasestorage.googleapis.com/v0/b/avaliakids.firebasestorage.app/o/logo.png?alt=media',
     },
+
     plugins: [
       'expo-router',
-      [
-        'expo-splash-screen',
-        {
-          image: 'https://firebasestorage.googleapis.com/v0/b/avaliakids.firebasestorage.app/o/logo.png?alt=media',
-          imageWidth: 200,
-          resizeMode: 'contain',
-          backgroundColor: '#ffffff',
-        },
-      ],
     ],
+
     experiments: {
       typedRoutes: true,
     },
+
     extra: {
       apiBaseUrl: process.env.API_BASE_URL,
     },
