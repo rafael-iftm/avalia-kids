2e1ae555278fa163bc815f171c05a26310449e0c
prado-baasic <142061101+rafael-iftm@users.noreply.github.com>
2025-05-26 22:41:40 -0300

    feat: add feign config

--- backend/quiz-service/src/main/java/com/avaliakids/quiz/clients/QuestionClient.java
+++ backend/quiz-service/src/main/java/com/avaliakids/quiz/clients/QuestionClient.java
@@ -4,9 +4,10 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import com.avaliakids.quiz.config.FeignConfig;
 import com.avaliakids.quiz.dto.QuestionDTO;
 
-@FeignClient(name = "api-gateway")
+@FeignClient(name = "api-gateway", configuration = FeignConfig.class)
 public interface QuestionClient {
     @GetMapping("/question-service/questions/id/{id}")
     QuestionDTO getQuestionById(@PathVariable("id") String id);
